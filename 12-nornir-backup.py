fad54632e4ada3426656aa30dc6ae33617a94669
Jag <jagadnag@gmail.com>
2022-06-12 15:53:30 -0400

    updated

--- 12-nornir-backup.py
+++ 12-nornir-backup.py
@@ -6,20 +6,23 @@ from nornir_utils.plugins.functions import print_result
 
 BACKUP_DIR = "backup/"
 
-nr = InitNornir(config_file="./config.yml")
+# Initiate nornir inventory
+nr = InitNornir(
+    config_file="config.yml", dry_run=True
+)
 
-
+# Function to Create backup folder
 def create_backups_dir():
     if not os.path.exists(BACKUP_DIR):
         os.mkdir(BACKUP_DIR)
 
-
+# Function to save the device configs to file
 def save_config_to_file(hostname, config):
     filename =  f"{hostname}.txt"
     with open(os.path.join(BACKUP_DIR, filename), "w") as f:
         f.write(config)
 
-
+# Function to collect the show run from devices
 def get_netmiko_backups():
     backup_results = nr.run(
         task=netmiko_send_command, 
@@ -32,6 +35,7 @@ def get_netmiko_backups():
             config=backup_results[hostname][0].result,
         )
 
+# Main function to invoke other functions
 def main():
     create_backups_dir()
     get_netmiko_backups()
